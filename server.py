32c813536579b29979aab26d3f0112fa547a79f7
Ashwathi Subramanian <subramanian.ashwathi@gmail.com>
2023-06-07 20:55:47 -0600

    add time

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+# print("Current Time =", current_time)
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "<h1>Hello</h1>"
+  return "<h1>Hello the current time is: " + current_time + "</h1>"
 
 if __name__ == "__main__":
   app.run(debug=True)
